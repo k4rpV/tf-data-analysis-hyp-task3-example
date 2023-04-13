0a8d4065b77064c02de4c80f17052115b6ae74df
k4rpV <101994618+k4rpV@users.noreply.github.com>
2023-04-13 18:42:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,4 +4,4 @@ import numpy as np
 chat_id = 953761952
 
 def solution(x: np.array) -> bool:
-    return ztest(x1=x, value=500, alternative='larger')[1] > 0.1
+    return ztest(x1=x, value=500, alternative='larger')[1] < 0.1
